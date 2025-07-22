53699470a799d57d4f7cfeb5ede162a3447b8abc
sarfraz alam <sarfrazk7858@gmail.com>
2025-07-22 10:11:31 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="text-5xl text-dark-100  font-bold">Welcome</Text>
+      <Link href={"/onboarding"}>
+        Onboarding
+      </Link>
+      <Link href={"/movie/avengers"}>
+        Avenger Movie
+      </Link>
+    </View>
+  );
+}
